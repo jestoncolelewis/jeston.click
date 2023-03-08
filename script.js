d6988ddf6dff943a2f1ba188cdffc75f50a92e49
Jeston Lewis <jeston.lewis@protonmail.com>
2023-03-08 16:11:59 -0700

    built fetch for test function

--- script.js
+++ script.js
@@ -3,19 +3,16 @@ const left_div = document.querySelector('#page-left');
 const count_element = document.createElement('p');
 
 // get page count from db
-let page_count = localStorage.getItem('page_view'); // from db
+fetch("https://0cxjlr27zj.execute-api.us-west-2.amazonaws.com/my-function")
+.then((response) => response.text())
+.then((data) => {
+    // display page count on index.html
+    count_element.textContent = data;
+    left_div.append(count_element);
+});
 
 // detect new page visitor & update locally
-if (page_count) {
-    page_count = Number(page_count) + 1;
-    localStorage.setItem('page_view', page_count)
-} else {
-    page_count = 1;
-    localStorage.setItem('page_view', 1)
-}
 
-// display page count on index.html
-count_element.textContent = page_count;
-left_div.append(count_element);
+
 
 // put new page count to db
