4439d8fb03cf397a483df7bdc47253fa7ffdd3b9
Jeston Lewis <jeston.lewis@protonmail.com>
2023-03-07 19:05:51 -0700

    built local storage version of page counter

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+// initialize variables
+const left_div = document.querySelector('#page-left');
+const count_element = document.createElement('p');
+
 // get page count from db
+let page_count = localStorage.getItem('page_view'); // from db
 
-// detect new page visitor
-
-// update page count locally
+// detect new page visitor & update locally
+if (page_count) {
+    page_count = Number(page_count) + 1;
+    localStorage.setItem('page_view', page_count)
+} else {
+    page_count = 1;
+    localStorage.setItem('page_view', 1)
+}
 
 // display page count on index.html
+count_element.textContent = page_count;
+left_div.append(count_element);
 
 // put new page count to db
