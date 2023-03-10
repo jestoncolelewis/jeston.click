70f4717babc8d4d5b0d02ed7d28d46d8f7cd5386
Jeston Lewis <jeston.lewis@protonmail.com>
2023-03-09 19:19:07 -0700

    added functionality to increase page count

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 // initialize variables
 const left_div = document.querySelector('#page-left');
 const count_element = document.createElement('p');
+const url = 'https://0cxjlr27zj.execute-api.us-west-2.amazonaws.com/my-function'
 
 // get page count from db
-fetch("https://0cxjlr27zj.execute-api.us-west-2.amazonaws.com/my-function")
+fetch(url)
 .then((response) => response.text())
 .then((data) => {
     // display page count on index.html
     count_element.textContent = data;
     left_div.append(count_element);
+    
+    // detect new page visitor & update locally
+    let new_count = Math.floor(data) + 1;
+    
+    // put new page count to db
+    /* fetch(url, {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'applications/json'
+        },
+        body: new_count
+    }); */
 });
-
-// detect new page visitor & update locally
-
-
-
-// put new page count to db
