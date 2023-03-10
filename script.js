e68b646e208a7eefc3f794f9234ef2c119ad9f8a
Jeston Lewis <jeston.lewis@protonmail.com>
2023-03-09 19:31:44 -0700

    styled page count and added title

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // initialize variables
-const left_div = document.querySelector('#page-left');
+const footer = document.querySelector('footer');
 const count_element = document.createElement('p');
+const count_title = document.createElement('p')
 const url = 'https://0cxjlr27zj.execute-api.us-west-2.amazonaws.com/my-function'
 
 // get page count from db
@@ -8,8 +9,12 @@ fetch(url)
 .then((response) => response.text())
 .then((data) => {
     // display page count on index.html
+    count_title.textContent = 'Page Views';
+    count_title.textAlign = 'center';
     count_element.textContent = data;
-    left_div.append(count_element);
+    count_element.style.textAlign = 'center';
+    footer.append(count_title);
+    footer.append(count_element);
     
     // detect new page visitor & update locally
     let new_count = Math.floor(data) + 1;
