36d483c59fd5c5be44e97d4b221723cc0434ec33
Jeston Lewis <jeston.lewis@protonmail.com>
2023-03-11 15:01:08 -0700

    created table building code

--- iac/setup.py
+++ iac/setup.py
@@ -1,5 +1,32 @@
 import boto3
 
 # resources to setup
-dynamodb = boto3.resource('dynamodb')
-lambda_function = boto3.client('lamba')
+dynamodb = boto3.client('dynamodb')
+
+table = dynamodb.create_table(
+    TableName = 'page_views',
+    KeySchema = [
+        {
+            'AttributeName': 'view_num',
+            'KeyType': 'HASH'
+        },
+        {
+            'AttributeName': 'date_time',
+            'KeyType': 'RANGE'
+        },
+    ],
+    AttributeDefinitions = [
+        {
+            'AttributeName': 'view_num',
+            'AttributeType': 'N'
+        },
+        {
+            'AttributeName': 'date_time',
+            'AttributeType': 'N'
+        },
+    ],
+    ProvisionedThroughput = {
+        'ReadCapacityUnits': 5,
+        'WriteCapacityUnits': 5
+    }
+)
