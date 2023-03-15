23dd6a39f1369785ced5fd808833826fcef0d7f7
Jeston Lewis <jeston.lewis@protonmail.com>
2023-03-14 18:08:07 -0600

    completed lambda function

--- iac/index.py
+++ iac/index.py
@@ -1,27 +1,20 @@
 import boto3
 import json
 
-dynamodb = boto3.client('dynamodb')
-table = dynamodb.Table('page_views')
+dynamodb = boto3.client("dynamodb")
+table = dynamodb.Table("page_views")
 
-Key = {
-    'view_num':'',
-    'date_time':''
-}
+Key = {"view_num": "", "date_time": ""}
+
 
 def lambda_handler(event, context):
-    if event.routekey == 'GET /items':
+    if event.get("routeKey") == "GET /items":
         response = table.get_item(Key)
-        item = response['Item']
+        item = response["Item"]
         return item
-    elif event.routekey == 'PUT /items':
+    if event.get("routeKey") == "PUT /items":
         response = table.update_item(
             Key,
-            UpdateExpression = 'SET view_num = :vall',
-            ExpressionAttributeValues = {
-                ':vall': 2
-            }
+            UpdateExpression="SET view_num = :vall",
+            ExpressionAttributeValues={":vall": item},
         )
-    else:
-        error = 'Error'
-        return error
