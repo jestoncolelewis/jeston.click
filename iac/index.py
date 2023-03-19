857069ef1067af8e365144a92febac84d6c510d5
Jeston Lewis <jeston.lewis@protonmail.com>
2023-03-18 19:41:42 -0700

    replaced the put method with an update method

--- iac/index.py
+++ iac/index.py
@@ -10,12 +10,11 @@ table = dynamor.Table(name)
 now = datetime.datetime.now().timestamp()
 now = "{}".format(now)
 
-# how do I get the latest item?
-key = {"date-time": {"N": "1679179226.926753"}}
+key = {"primary-key": {"N": "0"}}
 
 
 def lambda_handler(event, context):
-    if event.get("routeKey") == "GET /items":
+    if event.get("routeKey") == "GET /jestondotclick-page-views":
         response = dynamodb.get_item(
             TableName = name,
             Key = key
@@ -26,15 +25,17 @@ def lambda_handler(event, context):
         item += 1
         item = "{}".format(item)
         
-        response = dynamodb.put_item(
+        response = dynamodb.update_item(
             TableName = name,
-            Item = {
-                "date-time": {
-                    "N": now
-                },
-                "view-num": {
-                    "N": item
-                }
+            Key = key,
+            ExpressionAttributeNames = {
+                "#DT": "date-time",
+                "#VN": "view-num"
             },
+            ExpressionAttributeValues = {
+                ":d": {"N": now},
+                ":v": {"N": item}
+            },
+            UpdateExpression = "SET #DT = :d, #VN = :v",
         )
         return view
