15fc09eb60a72931da43a919742579ad61bc59ad
Jeston Lewis <jeston.lewis@protonmail.com>
2023-08-04 16:17:11 -0600

    added imports and construct calls

--- jestonclick/jestonclick_stack.py
+++ jestonclick/jestonclick_stack.py
@@ -1,19 +1,14 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
 from constructs import Construct
+from aws_cdk import (
+    Stack
+)
+from .hosting import Hosting
+from .counts import Counts
 
 class JestonClickStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "JestonClickQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        hosting = Hosting(self, "Hosting")
+        form = Counts(self, "Form")
