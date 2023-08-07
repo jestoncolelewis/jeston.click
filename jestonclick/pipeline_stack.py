301a4d430705c896c113404e884b58575cec2425
Jeston Lewis <jeston.lewis@protonmail.com>
2023-08-06 18:21:26 -0600

    basic pipeline stack construct

--- jestonclick/pipeline_stack.py
+++ jestonclick/pipeline_stack.py
@@ -8,9 +8,22 @@ from aws_cdk.pipelines import (
     CodePipelineSource,
     ShellStep
 )
+from .buckets import name
 
 class ProdPipeline(Construct):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        
+        pipeline = CodePipeline(
+            self, "Pipeline",
+            pipeline_name="FrontEndPipeline",
+            synth=ShellStep(
+            "Synth",
+            input=CodePipelineSource.git_hub("jestoncolelewis/{}".format(name), "main"),
+            commands= [
+                "npm install 0g aws-cdk",
+                "python -m pip install -r requirements.txt",
+                "cdk synth"
+            ]
+            )
+        )
