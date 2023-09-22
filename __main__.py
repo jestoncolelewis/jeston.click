69acb7d61cabdae74ae210d12974955fcde2d4b8
Jeston Lewis <jeston.lewis@protonmail.com>
2023-09-22 16:18:09 -0600

    added cdn

--- __main__.py
+++ __main__.py
@@ -52,3 +52,63 @@ www_bucket = aws.s3.Bucket(
         redirect_all_requests_to=website_name
     )
 )
+
+cdn = aws.cloudfront.Distribution(
+    "cdn",
+    enabled=True,
+    origins=[
+        aws.cloudfront.DistributionOriginArgs(
+            origin_id=main_bucket.arn,
+            domain_name=main_bucket.website_endpoint,
+            custom_origin_config=aws.cloudfront.DistributionOriginCustomOriginConfigArgs(
+                origin_protocol_policy="http-only",
+                http_port=80,
+                https_port=443,
+                origin_ssl_protocols=["TLSv1.2"],
+            ),
+        )
+    ],
+    default_cache_behavior=aws.cloudfront.DistributionDefaultCacheBehaviorArgs(
+        target_origin_id=main_bucket.arn,
+        viewer_protocol_policy="redirect-to-https",
+        allowed_methods=[
+            "GET",
+            "HEAD",
+            "OPTIONS",
+        ],
+        cached_methods=[
+            "GET",
+            "HEAD",
+            "OPTIONS",
+        ],
+        default_ttl=600,
+        max_ttl=600,
+        min_ttl=600,
+        forwarded_values=aws.cloudfront.DistributionDefaultCacheBehaviorForwardedValuesArgs(
+            query_string=True,
+            cookies=aws.cloudfront.DistributionDefaultCacheBehaviorForwardedValuesCookiesArgs(
+                forward="all",
+            ),
+        ),
+    ),
+    price_class="PriceClass_100",
+    custom_error_responses=[
+        aws.cloudfront.DistributionCustomErrorResponseArgs(
+            error_code=404,
+            response_code=404,
+            response_page_path=f"/{error_document}",
+        )
+    ],
+    restrictions=aws.cloudfront.DistributionRestrictionsArgs(
+        geo_restriction=aws.cloudfront.DistributionRestrictionsGeoRestrictionArgs(
+            restriction_type="none",
+        ),
+    ),
+    viewer_certificate=aws.cloudfront.DistributionViewerCertificateArgs(
+        cloudfront_default_certificate=True,
+    ),
+)
+
+
+# Outputs
+pulumi.export("cdnURL", pulumi.Output.concat("https://", cdn.domain_name))
