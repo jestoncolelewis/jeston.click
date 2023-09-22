eb6a44524547eb6b1492a6d81a712440c8667e0a
Jeston Lewis <jeston.lewis@protonmail.com>
2023-09-22 15:58:38 -0600

    added buckets

--- __main__.py
+++ __main__.py
@@ -3,7 +3,25 @@
 import pulumi
 import pulumi_aws as aws
 
+path = "./www"
+website_name = "jeston.click"
+
 zone = aws.route53.Zone("zone",
     comment="",
-    name="jeston.click",
+    name=website_name,
     opts=pulumi.ResourceOptions(protect=True))
+
+main_bucket = aws.s3.Bucket(
+    website_name,
+    website=aws.s3.BucketWebsiteArgs(
+        index_document="index.html",
+        error_document="404error.html"
+    )
+)
+
+www_bucket = aws.s3.Bucket(
+    "www.{}".format(website_name),
+    website=aws.s3.BucketWebsiteArgs(
+        redirect_all_requests_to=website_name
+    )
+)
