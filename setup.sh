f2161d0e4281202633ba06b837a8c86d470ee0ea
Jeston Lewis <jeston.lewis@protonmail.com>
2023-10-01 19:00:58 +0100

    addedrecorsd boiler plate

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
+touch records.py
+echo -e 'txt = [""]\nmx = [""]\n\ndkim1 = [""]\ndkim2 = [""]\ndkim3 = [""]\n\ndmarc = [""]' >> records.py
